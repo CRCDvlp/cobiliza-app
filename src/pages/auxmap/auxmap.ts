a92c39c92fa6cf4583561fffed01e658f5dfaf60
Suzu Nohara <suzu.nkdca@gmail.com>
2018-03-20 22:53:02 -0600

    Formularios de configuracion

--- src/pages/auxmap/auxmap.ts
+++ src/pages/auxmap/auxmap.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angular';
+import { Geolocation } from '@ionic-native/geolocation';
+import { AlertController } from 'ionic-angular';
+import { LoadingController } from 'ionic-angular';
 
 @IonicPage()
 @Component({
@@ -8,16 +11,43 @@ import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angul
 })
 export class AuxmapPage {
 
-  lat: number = 51.678418;
-  lng: number = 7.809007;
+  lng:number = 19.429930;
+  lat:number = -99.148423;
   accion:string = "Nueva ubicacion";
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public viewCtrl: ViewController) {
+  constructor(public navCtrl: NavController, 
+              public navParams: NavParams, 
+              public viewCtrl: ViewController, 
+              public geolocation: Geolocation, 
+              public alertCtrl: AlertController,
+              public loadingCtrl: LoadingController) {
     this.accion = navParams.data.accion;
+    this.locatePhone();
   }
 
   dismissModal(){
     this.viewCtrl.dismiss({ mensaje: "Eres un pervertido sin remedio" });
+  }
+
+  locatePhone(){
+    this.presentLoading({
+      content: "Buscando tu posición",
+      duracion: 2000
+    });
+    this.geolocation.getCurrentPosition().then((resp) => {
+      this.lat = resp.coords.latitude;
+      this.lng = resp.coords.longitude;
+      console.log(this.lat);
+      console.log(this.lng);
+    }).catch((error) => {
+      this.showAlert({
+        title: "Error",
+        subtitle: "No pudimos obtener tu ubicación, verifica que tengas activada tu ubicación o que estes conectado a alguna red",
+        buttons: [
+          'OK'
+        ]
+      });
+    });
   }
 
   saveUbication(){
@@ -27,5 +57,22 @@ export class AuxmapPage {
   selectUbicacion(event){
     this.lat = event.coords.lat;
     this.lng = event.coords.lng;
+  }
+
+  showAlert(content:any) {
+    let alert = this.alertCtrl.create({
+      title: content.title,
+      subTitle: content.subtitle,
+      buttons: content.buttons
+    });
+    alert.present();
+  }
+
+  presentLoading(content:any) {
+    let loader = this.loadingCtrl.create({
+      content: content.contenido,
+      duration: content.duracion
+    });
+    loader.present();
   }
 }
--- src/pages/viajar/viajar.ts
+++ src/pages/viajar/viajar.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 import { Geolocation } from '@ionic-native/geolocation';
+import { AlertController } from 'ionic-angular';
+import { LoadingController } from 'ionic-angular';
+
 
 @IonicPage()
 @Component({
@@ -10,10 +13,14 @@ import { Geolocation } from '@ionic-native/geolocation';
 })
 export class ViajarPage {
 
-  lng:number;
-  lat:number;
+  lng:number = 19.429930;
+  lat:number = -99.148423;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public geolocation: Geolocation) {
+  constructor(public navCtrl: NavController, 
+              public navParams: NavParams, 
+              public geolocation: Geolocation, 
+              public alertCtrl: AlertController,
+              public loadingCtrl: LoadingController) {
     this.locatePhone();
   }
 
@@ -23,14 +30,41 @@ export class ViajarPage {
   }
 
   locatePhone(){
+    this.presentLoading({
+      contenido: "Buscando tu posición",
+      duracion: 2000
+    });
     this.geolocation.getCurrentPosition().then((resp) => {
       this.lat = resp.coords.latitude;
       this.lng = resp.coords.longitude;
       console.log(this.lat);
       console.log(this.lng);
     }).catch((error) => {
-      console.log('Error getting location', error);
+      this.showAlert({
+        title: "Error",
+        subtitle: "No pudimos obtener tu ubicación, verifica que tengas activada tu ubicación o que estes conectado a alguna red",
+        buttons: [
+          'OK'
+        ]
+      });
     });
+  }
+
+  showAlert(content:any) {
+    let alert = this.alertCtrl.create({
+      title: content.title,
+      subTitle: content.subtitle,
+      buttons: content.buttons
+    });
+    alert.present();
+  }
+
+  presentLoading(content:any) {
+    let loader = this.loadingCtrl.create({
+      content: content.contenido,
+      duration: content.duracion
+    });
+    loader.present();
   }
 
 }
