991af95b469b42ba7f93682b3d30bee8831a2b1b
Suzu Nohara <suzu.nkdca@gmail.com>
2018-03-21 21:56:25 -0600

    Interfaz de viajes terminados

--- src/pages/programados/programados.ts
+++ src/pages/programados/programados.ts
@@ -1,19 +1,18 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the ProgramadosPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
 @IonicPage()
 @Component({
   selector: 'page-programados',
   templateUrl: 'programados.html',
 })
 export class ProgramadosPage {
+
+  public event = {
+    month: '1990-02-19',
+    timeStarts: '07:43',
+    timeEnds: '1990-02-20'
+  }
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
